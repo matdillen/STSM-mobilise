aa76b4cd8eeff4c7903b7982439d2cf36c9e5486
Mathias Dillen <37537540+matdillen@users.noreply.github.com>
2019-10-08 16:58:25 +0100

    Update find-all-collectors.R
    added BG-Base collector matching and SPARQL queries for publication data for wikidata people, among other things.

--- find-all-collectors.R
+++ find-all-collectors.R
@@ -9,7 +9,7 @@ querki <- function(query,h="text/csv") {
   require(httr)
   response <- httr::GET(url = "https://query.wikidata.org/sparql", 
                         query = list(query = query),
-                        httr::add_headers(Accept = h))
+                        httr::add_headers(Accept = h),httr::user_agent("Matdillen"))
   return(httr::content(response,type=h,col_types = cols(.default = "c")))
 }
 
@@ -32,63 +32,73 @@ raw = list()
 iter = 1
 
 #entomologists of the world
-query <- 'SELECT ?item ?itemLabel ?entom_id ?orcid ?viaf ?isni WHERE {
+query <- 'SELECT ?item ?itemLabel ?entom_id ?orcid ?viaf ?isni ?yob ?yod WHERE {
   ?item wdt:P5370 ?entom_id. #entomologists of the world
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" } 
   OPTIONAL { ?item wdt:P496 ?orcid .}
   OPTIONAL { ?item wdt:P214 ?viaf .}
   OPTIONAL { ?item wdt:P213 ?isni .}
+  OPTIONAL { ?item wdt:P569 ?dob . BIND(YEAR(?dob) as ?yob) }
+	OPTIONAL { ?item wdt:P570 ?dod . BIND(YEAR(?dod) as ?yod) }
 }'
 raw[[iter]] = querki(query)
 iter = iter + 1
 
 #BHL creator
-query <- 'SELECT ?item ?itemLabel ?bhl_id ?orcid ?viaf ?isni WHERE {
+query <- 'SELECT ?item ?itemLabel ?bhl_id ?orcid ?viaf ?isni ?yob ?yod WHERE {
   ?item wdt:P4081 ?bhl_id. #BHL creator
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" } 
   OPTIONAL { ?item wdt:P496 ?orcid .}
   OPTIONAL { ?item wdt:P214 ?viaf .}
   OPTIONAL { ?item wdt:P213 ?isni .}
+  OPTIONAL { ?item wdt:P569 ?dob . BIND(YEAR(?dob) as ?yob) }
+	OPTIONAL { ?item wdt:P570 ?dod . BIND(YEAR(?dod) as ?yod) }
 }'
 #raw[[iter]] = query_wikidata(query)
 raw[[iter]] = querki(query)
 iter = iter + 1
 
 #Harvard
-query <- 'SELECT ?item ?itemLabel ?harv_id ?orcid ?viaf ?isni WHERE {
+query <- 'SELECT ?item ?itemLabel ?harv_id ?orcid ?viaf ?isni ?yob ?yod WHERE {
   ?item wdt:P6264 ?harv_id. #Harvard index of botanists
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" } 
   OPTIONAL { ?item wdt:P496 ?orcid .}
   OPTIONAL { ?item wdt:P214 ?viaf .}
   OPTIONAL { ?item wdt:P213 ?isni .}
+  OPTIONAL { ?item wdt:P569 ?dob . BIND(YEAR(?dob) as ?yob) }
+	OPTIONAL { ?item wdt:P570 ?dod . BIND(YEAR(?dod) as ?yod) }
 }'
 raw[[iter]] = querki(query)
 iter = iter + 1
 
 #Zoobank
-query <- 'SELECT ?item ?itemLabel ?zoo_id ?orcid ?viaf ?isni WHERE {
+query <- 'SELECT ?item ?itemLabel ?zoo_id ?orcid ?viaf ?isni ?yob ?yod WHERE {
   ?item wdt:P2006 ?zoo_id. #zoobank
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" } 
   OPTIONAL { ?item wdt:P496 ?orcid .}
   OPTIONAL { ?item wdt:P214 ?viaf .}
   OPTIONAL { ?item wdt:P213 ?isni .}
+  OPTIONAL { ?item wdt:P569 ?dob . BIND(YEAR(?dob) as ?yob) }
+	OPTIONAL { ?item wdt:P570 ?dod . BIND(YEAR(?dod) as ?yod) }
 }'
 raw[[iter]] = querki(query)
 iter = iter + 1
 
 #IPNI
-query <- 'SELECT ?item ?itemLabel ?ipni_id ?orcid ?viaf ?isni WHERE {
+query <- 'SELECT ?item ?itemLabel ?ipni_id ?orcid ?viaf ?isni ?yob ?yod WHERE {
   ?item wdt:P586 ?ipni_id. #ipni
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" } 
   OPTIONAL { ?item wdt:P496 ?orcid .}
   OPTIONAL { ?item wdt:P214 ?viaf .}
   OPTIONAL { ?item wdt:P213 ?isni .}
+  OPTIONAL { ?item wdt:P569 ?dob . BIND(YEAR(?dob) as ?yob) }
+	OPTIONAL { ?item wdt:P570 ?dod . BIND(YEAR(?dod) as ?yod) }
 }'
 raw[[iter]] = querki(query)
 iter = iter + 1
 
 #wikispecies
-query <- 'SELECT ?item ?itemLabel ?article ?orcid ?viaf ?isni
+query <- 'SELECT ?item ?itemLabel ?article ?orcid ?viaf ?isni ?yob ?yod
 WHERE
 {
 	?item wdt:P31 wd:Q5 .
@@ -98,6 +108,8 @@ WHERE
 	OPTIONAL { ?item wdt:P496 ?orcid .}
   OPTIONAL { ?item wdt:P214 ?viaf .}
   OPTIONAL { ?item wdt:P213 ?isni .}
+  OPTIONAL { ?item wdt:P569 ?dob . BIND(YEAR(?dob) as ?yob) }
+	OPTIONAL { ?item wdt:P570 ?dod . BIND(YEAR(?dod) as ?yod) }
 }'
 raw[[iter]] = querki(query)
 iter = iter + 1
@@ -179,6 +191,7 @@ View(filter(allb.names,id==item))
 
 allb.names$sname = gsub("^(.*[\\s])","",allb.names$itemLabel,perl=T)
 
+#try the IPNI beta API
 ipni_info = list()
 for (i in 1:dim(raw[[5]])[1]) {
   ipni_info[[i]] = fromJSON(paste0("https://beta.ipni.org/api/1/a/urn:lsid:ipni.org:authors:",raw[[5]]$ipni_id[i]))
@@ -187,3 +200,201 @@ for (i in 1:dim(raw[[5]])[1]) {
   }
 }
 #timed out after 317 requests
+#evening: after 2933 requests
+#maybe with trycatch, but will take hours!
+
+
+####Match BG-Base collectors to wikidata items
+setwd("D:/apm/STSM mobilise/data")
+bgb = read_csv("COLLECTORS_24APR19.csv",locale = locale(encoding = "latin1"))
+#View(filter(allb.names,sname%in%bgb$LNAME))
+
+#make date ranges
+allb.names$date = paste(allb.names$yob,allb.names$yod,sep="-")
+allb.names$date = gsub("NA","",allb.names$date)
+
+#extract year
+for (i in 1:dim(bgb)[1]) {
+  bgb$date1[i] = substr(bgb$BIRTH_DT[i],nchar(bgb$BIRTH_DT[i])-3,nchar(bgb$BIRTH_DT[i]))
+  bgb$date2[i] = substr(bgb$DEATH_DT[i],nchar(bgb$DEATH_DT[i])-3,nchar(bgb$DEATH_DT[i]))
+}
+bgb$date = paste(bgb$date1,bgb$date2,sep="-")
+bgb$date = gsub("NA","",bgb$date)
+bgb$date[bgb$date=="-"]="NA"
+
+
+#try wikidata api
+# allb.names$id = gsub("http://www.wikidata.org/entity/","",allb.names$item)
+# tst50 = paste(allb.names$id[1:50],collapse="|")
+# test = fromJSON(paste0("https://www.wikidata.org/w/api.php?action=wbgetentities&ids=",tst50,"&format=json"))
+# 
+# library(httr)
+# tst100 = paste(allb.names$id[51:100],collapse="|")
+# test3 = httr::GET(url = paste0("https://www.wikidata.org/w/api.php?action=wbgetentities&ids=",tst100,"&format=json"),
+#                                httr::user_agent("Matdillen"))
+# test3.c = httr::content(test3,type="application/json")
+# 
+# for (i in seq(from=1,to=dim(allb.names)[1],by=50)) {
+#   #too big for memory, will be difficult to effectively parse
+# }
+
+bgb$fullname = paste(bgb$FNAME,bgb$MNAME,bgb$LNAME)
+bgb$fullname = gsub("NA ","",bgb$fullname)
+
+#View(filter(bgb,fullname%in%allb.names$itemLabel))
+
+#Find how many specimens a COLL_ID collected
+#based on latest BG-Base specimen export (imported as 'exp': data from May 2019)
+bgb$n = NA
+bgb$mndate = NA
+bgb$mxdate = NA
+for (i in 1:dim(bgb)[1]) {
+  k = speccoll[speccoll$COLL_ID==bgb$COLL_ID[i],]
+  if (is.na(bgb$date1[i])) {
+  dates = filter(exp,bgb$COLL_ID[i]==COLL_ID,is.na(COLL_DT)==F)$COLL_DT
+    if (length(dates)>0) {
+      bgb$mndate[i] = min(as.numeric(substr(dates,8,11)))
+      bgb$mxdate[i] = max(as.numeric(substr(dates,8,11)))
+    }
+  }
+  bgb$n[i]=k$n[1]
+  if (i%%100==0) {
+    print(i)
+  }
+}
+
+#kill ranges over a 100: probably an error in one of the dates or associations
+for (i in 1:dim(bgb)[1]) {
+  if (is.na(bgb$mndate[i])==F&is.na(bgb$mxdate[i])==F) {
+    dur = bgb$mxdate[i] - bgb$mndate[i]
+    if (dur > 100) {
+      bgb$mndate[i] = NA
+      bgb$mxdate[i] = NA
+      }
+  }
+}
+
+#only people who have collected specimens
+bgb2 = filter(bgb,n>0)
+
+#only those who have some sort of date
+bgb3 = filter(bgb2,is.na(date1)==F|is.na(mndate)==F)
+bgb3 = filter(bgb3,is.na(LNAME)==F) #remove no collector specified
+
+#add 15y margin for childhood
+bgb3$mndate2 = bgb3$mndate - 15
+
+bgb3$options = NA
+bgb3$lopt = NA
+
+#create initials for the wikidata item labels
+allb.names$init = gsub("\'","",allb.names$itemLabel,fixed=T)
+allb.names$init = gsub("\"","",allb.names$init,fixed=T)
+allb.names$init = gsub("(?<!\\s).","",allb.names$init,perl=T)
+allb.names$init = paste0(substr(allb.names$itemLabel,1,1),allb.names$init)
+
+#match (see documentation for more explanation)
+for (i in 1:dim(bgb3)[1]) {
+  truem = filter(allb.names,itemLabel==bgb3$fullname[i])
+  if (dim(truem)[1]>0) {
+    bgb3$lopt[i] = dim(truem)[1]
+    bgb3$options[i] = paste(truem$itemLabel,collapse="|")
+    next
+  }
+  yob = filter(allb.names,yob==bgb3$date1[i])
+  if (dim(yob)[1]>0) {
+    yob = filter(yob,agrepl(bgb3$LNAME[i],itemLabel))
+    len = dim(yob)[1]
+    if (is.na(bgb3$FNAME[i])==F) {
+      if (grepl(".",bgb3$FNAME[i],fixed=T)==F) {
+        yob = filter(yob,agrepl(bgb3$FNAME[i],itemLabel))
+        len = dim(yob)[1]
+      }
+      if (grepl(".",bgb3$FNAME[i],fixed=T)) {
+        yob = filter(yob,grepl(gsub("\\.","",bgb3$FNAME[i]),init))
+        len=dim(yob)[1]
+      }
+    }
+     if (len > 1&is.na(bgb3$MNAME[i])==F) {
+       yob = filter(yob,agrepl(bgb3$MNAME[i],itemLabel))
+       len = dim(yob)[1]
+     }
+    bgb3$lopt[i] = len
+    bgb3$options[i] = paste(yob$itemLabel,collapse="|")
+    print(i)
+    next
+  }
+  if (is.na(bgb3$mndate2[i])==F) {
+    #next
+    yob = filter(allb.names,yob<bgb3$mndate2[i])
+    yob = filter(yob,is.na(yod)|yod>bgb3$mxdate[i])
+    if (dim(yob)[1]>0) {
+      yob = filter(yob,agrepl(bgb3$LNAME[i],itemLabel))
+      len = dim(yob)[1]
+      if (is.na(bgb3$FNAME[i])==F) {
+        if (grepl(".",bgb3$FNAME[i],fixed=T)==F) {
+          yob = filter(yob,agrepl(bgb3$FNAME[i],itemLabel))
+          len = dim(yob)[1]
+        }
+        if (grepl(".",bgb3$FNAME[i],fixed=T)) {
+          yob = filter(yob,grepl(gsub("\\.","",bgb3$FNAME[i]),init))
+          len=dim(yob)[1]
+        }
+      }
+      if (len > 1&is.na(bgb3$MNAME[i])==F) {
+        yob = filter(yob,agrepl(bgb3$MNAME[i],itemLabel))
+        len = dim(yob)[1]
+      }
+      bgb3$lopt[i] = len
+      bgb3$options[i] = paste(yob$itemLabel,collapse="|")
+      print(i)
+      next
+    }
+  }
+}
+
+#unmatched
+allb.nd = filter(allb.names,is.na(yob))
+bgb4 = filter(bgb3,lopt!=1)
+
+
+#get pub info from wikidata
+# query <- paste0('SELECT ?item ?itemLabel ?pub WHERE 
+# {
+#   ?item wdt:P50 wd:',item,'.
+#   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" }
+#   OPTIONAL { ?item wdt:P577 ?pub.}
+#   }')
+# pubs = querki(query)
+
+#initialize
+pubst = tibble(item = 0,itemLabel = 0, pub = 0,id=0)
+
+#bombardment of sparql queries
+for (i in 10001:20000) {
+  query <- paste0('SELECT ?item ?itemLabel ?pub WHERE 
+{
+  ?item wdt:P50 wd:',allb.nd$id[i],'.
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" }
+  OPTIONAL { ?item wdt:P577 ?pub.}
+  }')
+  pubs = querki(query)
+  if (dim(pubs)[1]>0) {
+    pubs$id = allb.nd$id[i]
+    pubst = rbind(pubst,pubs)
+  }
+  print(i)
+}
+
+#post processing
+pubst.uni = filter(pubst,duplicated(id)==F)
+pubst.uni = pubst.uni[-1,]
+pubst2 = pubst
+pubst2$year = substr(pubst2$pub,1,4)
+pubst2 = pubst2[-1,]
+
+for (i in 1:dim(pubst.uni)[1]) {
+  giv = filter(pubst2,id==pubst.uni$id[i])
+  pubst.uni$mndate[i] = min(giv$year)
+  pubst.uni$mxdate[i] = max(giv$year)
+}
